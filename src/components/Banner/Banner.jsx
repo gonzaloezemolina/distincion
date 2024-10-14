b69490d9dad22f9ce29e0af56c150121f1fb8da9
gonzaloezemolina <gonzaloezemolina@gmail.com>
2024-10-14 17:36:53 -0300

    change title on banner and height on footer

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
         <section className='portada'>
             {/* <h1>¿No tenés el regalo de Mamá todavia?</h1> */}
             <h2>¡Sorprende a mamá con un buen desayuno!</h2>
-            <h3>15% OFF este 22 de octubre</h3>
+            <h3>15% OFF este 20 de octubre</h3>
         </section>
     </div>
   )
